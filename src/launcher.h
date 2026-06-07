1a390a1e9df10a08d8934167cd4ece9500860591
bswap64 <byteswap.c@gmail.com>
2026-06-07 17:27:47 +0400

    Chaned version from 1.1.5 to 1.1.6

--- src/launcher.h
+++ src/launcher.h
@@ -70,7 +70,7 @@ constexpr const char* DEFAULT_JVM_ARGS_OLD =
 constexpr const char* VERSIONS_MANIFEST_URL =
     "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json";
 
-constexpr const char* LAUNCHER_VERSION = "1.1.2";
+constexpr const char* LAUNCHER_VERSION = "1.1.6";
 
 struct DownloadEntry {
     std::string url;
